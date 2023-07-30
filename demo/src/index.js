6eb7f82840dbb4e1dc9f5fe10122eca840742928
蒲金波 <3443713935@qq.com>
2023-07-30 23:54:12 +0800

    first commit

---
+++ demo/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+// React.StrictMode  react的严格模式
+  <React.StrictMode>
+    <div>react1</div>
+  </React.StrictMode>
+);
